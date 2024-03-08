a7c45a75be0042dc8406061efcaff8806ce053b7
Hamid Tlailia <tlailia757@gmail.com>
2024-03-08 13:44:10 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,6 +18,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
+    methods : "POST" , "GET",
+    credentials :true
   },
 });
 
